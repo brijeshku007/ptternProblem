a718b9488e57ff15260ac3c21beebbe86b197315
brijesh007 <uietbrijesh@gmail.com>
2024-12-25 18:06:20 +0530

    intial commit

---
+++ PatternAll.java
@@ -0,0 +1,260 @@
+public class PatternAll {
+  // public static void main(String[] args) {
+  //   int n=5;
+  //   int nst=1;
+  //   for(int i=1;i<=n;i++){
+  //     for(int j=1;j<=nst;j++){
+  //       System.out.print("*"+" ");
+  //     }
+  //     nst++;
+  //     System.out.println();
+  //   }
+  // }
+
+//   public static void main(String[] args) {
+//     int n=5;
+//     int nst=5;
+//     for(int i=1;i<=n;i++){
+//       for(int j=1;j<=nst;j++){
+//         System.out.print("*"+" ");
+//       }
+//       System.out.println();
+//     }
+//   }
+// }
+
+// public static void main(String[] args) {
+//   int n=5;
+//   int nst=5;
+//   for(int i=1;i<=n;i++){
+//     for(int j=1;j<=nst;j++){
+//       System.out.print("*"+" ");
+//     }
+//     nst--;
+//     System.out.println();
+//   }
+
+// }
+
+
+// public static void main(String[] args) {
+//   int n=5;
+//   int nst=1;
+//   int count =1;
+//   for(int i=1;i<=n;i++){
+//     for(int j=1;j<=nst;j++){
+//       System.out.print(count+" ");
+//       count++;
+//     }
+//     nst++;
+//     System.out.println();
+//   }
+  
+// }
+
+
+// public static void main(String[] args) {
+//   int n=5;
+//   int nst=1;
+//   int count =1;
+//   for(int i=1;i<=n;i++){
+//     for(int j=1;j<=nst;j++){
+//       System.out.print(count+" ");
+//       count++;
+//     }
+//     count =1;
+//     nst++;
+//     System.out.println();
+//   }
+  
+// }
+
+// public static void main(String[] args) {
+//   int n=5;
+//   int nst=1;
+//   int count =1;
+//   for(int i=1;i<=n;i++){
+//     for(int j=1;j<=nst;j++){
+//       System.out.print(count+" ");
+//       count++;
+//     }
+//     count--;
+//     nst++;
+//     System.out.println();
+//   }
+  
+// }
+
+
+// public static void main(String[] args) {
+//   int n=5;
+//   int nst=1;
+//   char count ='A';
+//   for(int i=1;i<=n;i++){
+//     for(int j=1;j<=nst;j++){
+//       System.out.print(count+" ");
+//       count++;
+//     }
+//     nst++;
+//     System.out.println();
+//   }
+
+// public static void main(String[] args) {
+//   int n=5;
+//   int nst=1;
+//   int nsp= n;
+//   for(int i=1;i<=n;i++){
+//     for(int j=1;j<=nsp;j++){
+//       System.out.print(" "+" ");
+      
+//     }
+//     for(int k=1;k<=nst;k++){
+//       System.out.print("*"+" ");
+//     }
+//     nsp--;
+//     nst++;
+//     System.out.println();
+//   }
+  
+// }
+
+
+
+// public static void main(String[] args) {
+//   int n=5;
+//   int nst=n;
+//   int nsp= 0;
+//   for(int i=1;i<=n;i++){
+//     for(int j=1;j<=nsp;j++){
+//       System.out.print(" "+" ");
+      
+//     }
+//     for(int k=1;k<=nst;k++){
+//       System.out.print("*"+" ");
+//     }
+//     nsp++;
+//     nst--;
+//     System.out.println();
+//   }
+  
+// }
+
+
+
+// public static void main(String[] args) {
+//   int n=5;
+//   int nst=1;
+//   int nsp= n/2;
+//   for(int i=1;i<=n;i++){
+//     for(int j=1;j<=nsp;j++){
+//       System.out.print(" "+" ");
+      
+//     }
+//     for(int k=1;k<=nst;k++){
+//       System.out.print("*"+" ");
+//     }
+//     if(i<=n/2){
+//       nsp--;
+//       nst=nst+2;
+//     }else{
+//       nsp++;
+//       nst=nst-2;
+//     }
+    
+//     System.out.println();
+//   }
+  
+// }
+
+
+// public static void main(String[] args) {
+//   int n=5;
+//   int nst=1;
+//   int nsp= n/2;
+//   for(int i=1;i<=n/2+1;i++){
+//     for(int j=1;j<=nsp;j++){
+//       System.out.print(" "+" ");
+      
+//     }
+//     for(int k=1;k<=nst;k++){
+//       System.out.print("*"+" ");
+//     }
+   
+//       nsp--;
+//       nst=nst+2;
+   
+//     System.out.println();
+//   }
+  
+// }
+
+// public static void main(String[] args) {
+//   int n=5;
+//   int nst=n;
+//   int nsp=0;
+//   for(int i=1;i<=n;i++){
+//     for(int j=1;j<=nst;j++){
+//       System.out.print("*"+" ");
+//     }
+//     for(int k=1;k<=nsp;k++){
+//       System.out.print(" "+" ");
+//     }
+//     if(i<=n/2){
+//       nsp++;
+//       nst--;
+//     }else{
+//       nsp--;
+//       nst++;
+//     }
+//     System.out.println();
+//   }
+// }
+
+// public static void main(String[] args) {
+//   int n=9;
+//   int nst=n/2+1;
+//   int nsp=1;
+//   for(int i=1;i<=n;i++){
+//     for(int j=1;j<=nst;j++){
+//       System.out.print("*"+" ");
+//     }
+//     for(int k=1;k<=nsp;k++){
+//       System.out.print(" "+" ");
+//     }
+//     for(int l=1;l<=nst;l++){
+//       System.out.print("*"+" ");
+//     }
+//     if(i<=n/2){
+//       nsp=nsp+2;
+//       nst--;
+//     }else{
+//       nsp=nsp-2;
+//       nst++;
+//     }
+//     System.out.println();
+//   }
+// }
+  
+
+public static void main(String args[]){
+  int n=5;
+  int nst=1;
+  int nsp=4;
+  char count='A';
+  for(int i=1;i<=n;i++){
+    for(int j=1;j<=nsp;j++){
+      System.out.print(" "+" ");
+    }
+    for(int k=1;k<=nst;k++){
+      System.out.print(count+" ");
+      count++;
+    }
+    nsp--;
+    nst++;
+    count='A';
+    System.out.println();
+  }
+}
+}
+
+
